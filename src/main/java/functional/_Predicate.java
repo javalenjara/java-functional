e276d34482e1ce892a4e400490bc0a42a43ac48f
Jorge Valencia <jorge.a.valencia.-nd@disney.com>
2021-06-28 16:59:17 -0500

    Adding Predicates

---
+++ src/main/java/functional/_Predicate.java
@@ -0,0 +1,38 @@
+package functional;
+
+import java.util.function.Predicate;
+
+public class _Predicate {
+
+    public static void main(String[] args) {
+
+        System.out.println("Without Predicate");
+        System.out.println(isPhoneNumberValid("07000000000"));
+        System.out.println(isPhoneNumberValid("0700000000"));
+        System.out.println(isPhoneNumberValid("09009877300"));
+
+        System.out.println("With Predicate");
+        System.out.println(isPhoneNumberValidPredicate.test("07000000000"));
+        System.out.println(isPhoneNumberValidPredicate.test("0700000000"));
+        System.out.println(isPhoneNumberValidPredicate.test("09009877300"));
+
+        System.out.println("Is a valid phone number and contains number 3? " +
+                isPhoneNumberValidPredicate.and(containsNumber3).test("07009877300"));
+
+        System.out.println("Is a valid phone number and contains number 3? " +
+                isPhoneNumberValidPredicate.and(containsNumber3).test("07000000000"));
+
+        System.out.println("Is a valid phone number OR contains number 3? " +
+                isPhoneNumberValidPredicate.or(containsNumber3).test("07000000000"));
+    }
+
+    static boolean isPhoneNumberValid (String phoneNumber) {
+        return phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+    }
+
+    static Predicate<String> isPhoneNumberValidPredicate = phoneNumber ->
+            phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+    static Predicate<String> containsNumber3 = phoneNumber ->
+            phoneNumber.contains("3");
+}
