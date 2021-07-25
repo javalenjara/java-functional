77d76acc822dbff6c8d9c4447399a8784b046a23
Jorge Valencia <jorge.a.valencia.-nd@disney.com>
2021-07-24 23:19:58 -0500

    Optionals

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -4,10 +4,14 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Object value = Optional.ofNullable(null)
-                .orElseGet(() -> "default value");
+        /*Object value = Optional.ofNullable("hello")
+                .orElseGet(() -> "default value");*/
 
-        System.out.println(value);
+        /*Object value = Optional.ofNullable(null)
+                .orElseThrow(() -> new IllegalStateException("Exception"));*/
+
+       Optional.ofNullable(null)
+                .ifPresent(value -> System.out.println("Sending email to: " + value));
 
     }
 }
